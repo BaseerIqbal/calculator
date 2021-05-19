f474e6cccfb0f0955dd9f7be5caef338433f0719
BaseerIqbal <81772380+BaseerIqbal@users.noreply.github.com>
2021-05-19 07:26:11 +0530

    upload all files

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+function bSpace(){
+    var value = document.getElementById("display").value;
+    document.getElementById("display").value = value.substr(0, value.length - 1);
+    return value;
+}
